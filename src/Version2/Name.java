b739b00462abacbf891a476ab6c1a853fbc55c2d
Kyum <25101052@usc.edu.ph>
2026-09-10 12:23:00 +0800

    wa pa nahuman

---
+++ src/Version2/Name.java
@@ -0,0 +1,72 @@
+package Version2;
+
+public class Name {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+
+    public Name() {
+        firstName = "N/A";
+        middleName = "";
+        lastName = "N/A";
+        suffix = "";
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = "N/A";
+        this.lastName = lastName;
+        this.suffix = "";
+    }
+
+    public Name(String firstName, String middleName, String lastName, String suffix) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", suffix='" + suffix + '\'' +
+                '}';
+    }
+}
